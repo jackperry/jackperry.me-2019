b976b154d6fa391704e5bc17f0dc3c68eeeaa77b
Jack Perry <jack@jackperry.me>
2019-10-31 10:46:01 -0700

    form

--- resources/js/local/_example-partial.js
+++ resources/js/local/_example-partial.js
@@ -7,3 +7,15 @@ if ( !page('blog') )
     document.querySelector('.content > p').classList.add('intro')
 
 }
+
+$("#contact-form").submit(function(e) {
+  e.preventDefault();
+
+  var $form = $(this);
+  $form.slideUp();
+  $('#form-loading').fadeIn();
+  $.post($form.attr("action"), $form.serialize()).then(function() {
+    $('#form-loading').before('<div class="bg-green-lightest border border-green-light text-green-dark px-4 py-3 rounded" role="alert"><strong>Thanks for your message.</strong> I usually respond within 48 hours, so you should hear from me soon!</div>');
+    $('#form-loading').fadeOut();
+  });
+});  
