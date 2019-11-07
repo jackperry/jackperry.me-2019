ac72948c97a4036ee06f77f1de8d4ef99f0a2f20
Jack Perry <jack@jackperry.me>
2019-11-07 15:23:15 -0800

    form validation

--- javascript/main.js
+++ javascript/main.js
@@ -4042,16 +4042,46 @@ if (!page('blog')) {
   document.querySelector('.content > p').classList.add('intro');
 }
 
+function validateForm(form) {
+  var isFormValid = true;
+  var invalidClassName = 'error-input-gradient';
+  var inputs = form.find('input, select, textarea');
+  inputs.each(function () {
+    $(this).parent().removeClass(invalidClassName);
+
+    if ($(this)[0].checkValidity() == false) {
+      isFormValid = false;
+      $(this).parent().addClass(invalidClassName);
+    }
+  });
+
+  if (isFormValid == true) {
+    return true;
+  }
+}
+
+$('#contact-form input, #contact-form select, #contact-form textarea').on('blur', function () {
+  var invalidClassName = 'error-input-gradient';
+  $(this).parent().removeClass(invalidClassName);
+
+  if ($(this)[0].checkValidity() == false) {
+    $(this).parent().addClass(invalidClassName);
+  }
+});
 $("#contact-form").submit(function (e) {
   e.preventDefault();
-  var $form = $(this);
-  $form.slideUp();
-  $('#form-loading').fadeIn();
-  $.post($form.attr("action"), $form.serialize()).then(function () {
-    $('#form-loading').fadeOut('slow', function () {
-      $('#form-success').fadeIn();
+  var form = $(this);
+  var validForm = validateForm(form);
+
+  if (validForm) {
+    form.slideUp();
+    $('#form-loading').fadeIn();
+    $.post(form.attr("action"), form.serialize()).then(function () {
+      $('#form-loading').fadeOut('slow', function () {
+        $('#form-success').fadeIn();
+      });
     });
-  });
+  }
 });
 $('a[href="#contact"]').on('click', function (e) {
   e.preventDefault();
--- resources/js/local/_example-partial.js
+++ resources/js/local/_example-partial.js
@@ -8,17 +8,45 @@ if ( !page('blog') )
 
 }
 
+function validateForm( form ) {
+  var isFormValid = true;
+  var invalidClassName = 'error-input-gradient';
+  var inputs = form.find('input, select, textarea');
+  inputs.each(function() {
+    $(this).parent().removeClass(invalidClassName);
+    if ($(this)[0].checkValidity() == false) {
+      isFormValid = false;
+      $(this).parent().addClass(invalidClassName);
+    }
+  });
+
+  if (isFormValid == true) {
+    return true;
+  }
+}
+
+$('#contact-form input, #contact-form select, #contact-form textarea').on('blur', function() {
+  var invalidClassName = 'error-input-gradient';
+  $(this).parent().removeClass(invalidClassName);
+  if ($(this)[0].checkValidity() == false) {
+    $(this).parent().addClass(invalidClassName);
+  }
+});
+
 $("#contact-form").submit(function(e) {
   e.preventDefault();
 
-  var $form = $(this);
-  $form.slideUp();
-  $('#form-loading').fadeIn();
-  $.post($form.attr("action"), $form.serialize()).then(function() {
-    $('#form-loading').fadeOut('slow', function() {
-      $('#form-success').fadeIn();
+  let form = $(this);
+  let validForm = validateForm(form);
+  if (validForm) {
+    form.slideUp();
+    $('#form-loading').fadeIn();
+    $.post(form.attr("action"), form.serialize()).then(function() {
+      $('#form-loading').fadeOut('slow', function() {
+        $('#form-success').fadeIn();
+      });
     });
-  });
+  }
 });
 
 $('a[href="#contact"]').on('click', function(e) {
